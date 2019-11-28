87908d1072278eab3544778b765c96efbec58f3d
Wolfgang Müller <wolfgang.mueller@h-its.org>
2019-11-28 14:21:19 +0100

    Towards parsing, next steps. CODE NOT RUNNING BUT USEFUL.

---
+++ src/table_validator/validator_classes/NoError.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class NoError:
+    def __init__(self,row,column,value):
+        self.row = row;
+        self.column = column
+        self.value = value
+
+    def message(self):
+        return "Everything fine at %d, %d: %s" %(row,column,value)
--- src/table_validator/validator_classes/TypeSheetValidator.py
+++ src/table_validator/validator_classes/TypeSheetValidator.py
@@ -2,6 +2,14 @@
 
 
 class TypeSheetValidator(SheetValidator):
-    # TODO an integer/float/string must be present
-    def something():
-        return 0
+    def __init__(self,row,column,cls):
+        super(self,row,column)
+        self.cls = cls
+
+    def validate(self,value):
+        try:
+            self.cls(value)
+        except ValueError:
+            return False,ValidationErrorObject(row,column,"Wrong data type. Expected:", cls)
+        else:
+            return True,NoError(row,colum);
