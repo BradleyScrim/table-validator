75d721ab7ae8157a5463968283b0570d5270078a
winterb <winterb@uni-greifswald.de>
2019-11-29 09:39:15 +0100

    init SmartChemicalCompoundSheet*

---
+++ src/table_validator/validator_classes/SmartChemicalCompoundSheetError.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from .SheetError import SheetError
+
+class SmartChemicalCompoundSheetError(SheetError):
+    def __init__(self,row,column,value,cls):
+        super(self,row,column,value)
+        self.cls = cls
+
+    def message(self):
+        return "%d, %d: %s should have been of type %s" %(self.row,self.column,self.value,self.cls)
--- src/table_validator/validator_classes/SmartChemicalCompoundSheetValidator.py
+++ src/table_validator/validator_classes/SmartChemicalCompoundSheetValidator.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import SheetValidator
+from .SheetValidator import SheetValidator
+from .SmartChemicalCompoundSheetError import SmartChemicalCompoundSheetError
 
 class SmartChemicalCompoundSheetValidator(SheetValidator):
-# TODO checks that something is a compound
-    def somethingElse():
-        # FIXME implement
-        return 0
+    def __init__(self,row,column):
+        super().__init__(row,column)
+
+    # TODO checks that something is a compound
+    def validate(self,value):
+        if(value):
+            return True,None
+        else:
+            return False,SmartChemicalCompoundSheetError(self.row,self.column,self.value)
